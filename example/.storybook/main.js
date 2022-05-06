28cb31919f8f89452cd172cc6914dd08400bec98
Suhyun Park <me@shiftpsh.com>
2022-05-07 06:08:24 +0900

    fix: storybook

--- example/.storybook/main.js
+++ example/.storybook/main.js
@@ -1,10 +1,13 @@
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/preset-create-react-app',
+    // '@storybook/preset-create-react-app',
     'storybook-addon-styled-component-theme/dist/preset',
   ],
   framework: '@storybook/react',
