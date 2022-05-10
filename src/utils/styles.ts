af7ee59a48cd9266efb3a31da25c2c5b43cfdd01
RanolP <public.ranolp@gmail.com>
2022-05-10 23:50:45 +0900

    fix: invalid return type of cssVariables function

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,8 +1,25 @@
 import { css } from 'styled-components'
 import { SolvedTheme } from '../styles'
 
-export const toCssName = (name: string): string =>
-  name.replace(/[A-Z]/g, (m) => `-${m.toLowerCase()}`).replace(/^-/, '')
+export type MakeKebabCase<S extends string, ReturnQueue extends string = ''> =
+  // Separate string into first character T and rest string U, only works if length > 1
+  S extends `${infer T}${infer U}`
+  // If it's the first character of whole string, just lowercase first character
+  ? ReturnQueue extends ''
+  ? MakeKebabCase<U, Lowercase<T>>
+  // if it's uppercased character, append -${Lowercase<T>} into return queue
+  : T extends Uppercase<T>
+  ? MakeKebabCase<U, `${ReturnQueue}-${Lowercase<T>}`>
+  // or, just append itself
+  : MakeKebabCase<U, `${ReturnQueue}${T}`>
+  // It's else branch of the first length > 1 check
+  : `${ReturnQueue}${S}`
+
+export const toCssName = <S extends string>(name: S): MakeKebabCase<S> =>
+  name.replace(/[A-Z]/g, (m) => `-${m.toLowerCase()}`).replace(/^-/, '') as MakeKebabCase<S>
+
+export type VariableName<Prefix extends string, Name> =
+  `--solvedac-${MakeKebabCase<Prefix>}-${MakeKebabCase<Name extends string ? Name : string>}`
 
 export const cssVariables = <
   T extends {
@@ -13,22 +30,22 @@ export const cssVariables = <
   defaults: T,
   prefix: P
 ): {
-  vars: { [key in keyof T]: `--solvedac-${string}` }
-  v: { [key in keyof T]: `var(--solvedac-${P}-${string})` }
+  vars: { [K in keyof T]: VariableName<P, K> }
+  v: { [K in keyof T]: `var(${VariableName<P, K>})` }
   styles: (theme: SolvedTheme) => string
 } => {
-  const names = Object.keys(defaults)
+  const names = Object.keys(defaults) 
 
   const vars = Object.fromEntries(
     names.map((name) => [
-      name,
-      `--solvedac-${toCssName(prefix)}-${toCssName(name)}`,
-    ])
-  ) as { [key in keyof T]: `--solvedac-${string}` }
+      name ,
+      `--solvedac-${toCssName(prefix)}-${toCssName(name)}` ,
+    ] )
+  ) as { [K in keyof T]: VariableName<P, K> }
 
   const v = Object.fromEntries(
     Object.entries(vars).map(([k, v]) => [k, `var(${v})`])
-  ) as { [key in keyof T]: `var(--solvedac-${P}-${string})` }
+  )  as { [K in keyof T]: `var(${VariableName<P, K>})` }
 
   const styles = (theme: SolvedTheme): string =>
     (
