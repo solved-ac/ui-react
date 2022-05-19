e0379012597f32916a8775f12edb8915350ddd88
Suhyun Park <me@shiftpsh.com>
2022-05-19 22:57:27 +0900

    feat: TableContainer

---
+++ src/components/Table/TableContainer.tsx
@@ -0,0 +1,21 @@
+import React, { ElementType } from 'react'
+import styled from 'styled-components'
+import { PC, PP, PR } from '../../types/PolymorphicElementProps'
+
+const TableContainerContainer = styled.div`
+  overflow-x: auto;
+`
+
+export const TableContainer: PC<'div'> = React.forwardRef(
+  <T extends ElementType>(props: PP<T>, ref?: PR<T>) => {
+    const { as = 'table', ...rest } = props
+
+    return (
+      <TableContainerContainer
+        ref={ref}
+        as={as}
+        {...rest}
+      />
+    )
+  }
+)
--- src/components/Table/index.ts
+++ src/components/Table/index.ts
@@ -2,6 +2,7 @@ export * from './Cell'
 export * from './Row'
 export * from './Table'
 export * from './TableBody'
+export * from './TableContainer'
 export * from './TableFoot'
 export * from './TableHead'
 
