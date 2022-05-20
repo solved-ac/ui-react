ffabb0d2961ae3fa5d2f806762612209dce89f73
Suhyun Park <me@shiftpsh.com>
2022-05-20 17:32:15 +0900

    fix: types

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,26 +1,6 @@
 // eslint-disable-next-line react/no-typos
 import 'react'
 
-/**
- * Default CSS definition for typescript,
- * will be overridden with file-specific definitions by rollup
- */
-declare module '*.css' {
-  const content: { [className: string]: string }
-  export default content
-}
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface SvgrComponent
-  extends React.FunctionComponent<React.SVGAttributes<SVGElement>> {}
-
-declare module '*.svg' {
-  const svgUrl: string
-  const svgComponent: SvgrComponent
-  export default svgUrl
-  export { svgComponent as ReactComponent }
-}
-
 type CustomProp = { [key in `--${string}`]: string | undefined }
 declare module 'react' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
